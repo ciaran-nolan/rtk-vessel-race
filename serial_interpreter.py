ac75d86c038b1a10687e277851bf4293ea9919b8
ciarannolan <ciaran.nolan.3@ucdconnect.ie>
2019-12-16 19:28:52 +0000

    Structure changes

--- serial_interpreter.py
+++ serial_interpreter.py
@@ -1,6 +1,6 @@
 import serial
 import numpy as np
-import numpy.linalg as la
+import ubx_messages
 import struct
 import time
 import math
@@ -15,63 +15,37 @@ def createserialcommunication():
     return ser
 
 
-def ubxnavposllh(ser):
-    time.sleep(.1)
-    bytes2 = b'\xb5b\x01\x02\x1c\x00\x88l\xcf\x1d0\x16K\xfcL\xf6\xc5\x1fNY\x01\x00\xe3\x8a\x00\x00\xd0\x08\x00\x00\x1c' \
-            b'\x13\x00\x00 '
-    bytes = ser.read(34)
-    print(bytes)
-    headerbyte1 = bytes[0]
-    headerbyte2 = bytes[1]
-    overhead = bytes[2]
-    overhead2 = bytes[3]
-    message_size = int.from_bytes(bytes[4:6],"little")
-    timeweek = int.from_bytes(bytes[6:10],"little")
-    longitude = struct.unpack("<i",bytes[10:14])
-    revisedlongitude = longitude[0] * 1e-7
-    latitude = struct.unpack("<I",bytes[14:18])
-    #latitude = int.from_bytes(bytes[14:18],"little")
-    revisedlatitude = latitude[0]*1e-7
-    heightellipsoid = int.from_bytes(bytes[18:22],"little")
-    heightmsl = int.from_bytes(bytes[22:26],"little")
-    horiaccuracy = int.from_bytes(bytes[26:30],"little")
-    vertaccuracy = int.from_bytes(bytes[30:34],"little")
-    print(revisedlongitude)
-
-def ubxnavrelposned(ser):
-    bytes = ser.read(46)
-    #bytes2=b'\xb5b\x01<(\x00\x00\x00\x00\x00\x88r3"*\x01\x00\x00\xe5\x01\x00\x00B\xff\xff\xff68\xde\x00\xee\x0e\x00\x00'
-    #headerbyte1 = bytes[0]
-    #headerbyte2 = bytes[1]
-    #class_byte = bytes[0]
-    #id = bytes[1]
-    #message_size = int.from_bytes(bytes[2:4], "little")
-    #message_version = bytes[4]
-    #reserved = bytes[5]
-    #ref_station_id = bytes[6:8]
-    #timeweek = int.from_bytes(bytes[8:12], "little")
-    relposn = struct.unpack("<l",bytes[12:16])
-    relpose = struct.unpack("<l", bytes[16:20])
-    relposd = struct.unpack("<l", bytes[20:24])
-    relposhpn = np.uintc(bytes[24]) * 1e-2
-    relposhpe = np.uintc(bytes[25]) * 1e-2
-    relposhpd = np.uintc(bytes[26]) * 1e-2
-    #reserved2 = bytes[27]
-    #accn =int.from_bytes(bytes[28:32], "little")
-    #acce = int.from_bytes(bytes[32:36], "little")
-    #accd = int.from_bytes(bytes[36:40], "little")
-    #flags = bytes[40:44]
-    #check1 = bytes[44];
-    #check2 = bytes[45];
-
-    resolvedn = relposn[0] + relposhpn
-    resolvede = relpose[0] + relposhpe
-    resolvedd = relposd[0] + relposhpd
 
 
-    #lineardistance = math.sqrt(resolvedn**2+resolvede**2)/100
-    #print(lineardistance)
-    return [resolvedn, resolvede, resolvedd]
+
+
+#x1,y1,x2,y2 = end points of line segment
+#x3,y3 = point to calculate distance to
+def dist_line_segment(base_ned, boat_ned): # x3,y3 is the point
+
+    norm = base_ned[0] * base_ned[0] + base_ned[1] * base_ned[1]
+    u = ((boat_ned[0]) * base_ned[0] + (boat_ned[1]) * base_ned[1]) / float(norm)
+
+    if u > 1:
+        u = 1
+    elif u < 0:
+        u = 0
+
+    x = u * base_ned[0]
+    y = u * base_ned[1]
+
+    dx = x - boat_ned[0]
+    dy = y - boat_ned[1]
+
+    # Note: If the actual distance does not matter,
+    # if you only want to compare what this function
+    # returns to other results of this function, you
+    # can just return the squared distance instead
+    # (i.e. remove the sqrt) to gain a little performance
+
+    dist = (dx*dx + dy*dy)**.5
+    print(dist)
+    return dist
 
 
 def angle_between(v1, v2):
@@ -83,7 +57,7 @@ def angle_between(v1, v2):
     print(angle*(180/math.pi))
     return angle
 
-def has_crossed_line(base_vector, ser):
+def has_crossed_line(base_ned, ser):
     crossedline = False
 
     while not(crossedline):
@@ -91,9 +65,8 @@ def has_crossed_line(base_vector, ser):
         checknum = check_bytes(ser)
         while checknum != 1:
             checknum = check_bytes(ser)
-        rover_vector = ubxnavrelposned(ser)
-        angle = angle_between((base_vector[0],base_vector[1]), (rover_vector[0], rover_vector[1]))
-        #print(angle*(180/math.pi))
+        boat_ned = ubx_messages.ubxnavrelposned(ser)
+        angle = angle_between((base_ned[0], base_ned[1]), (boat_ned[0], boat_ned[1]))
         if angle < 0:
             crossedline = True;
 
@@ -111,7 +84,7 @@ def check_bytes(ser):
         while not(nmea_end):
             bytes.append(ser.read(1))
             print( int.from_bytes(bytes[i],"little"))
-            if  int.from_bytes(bytes[i],"little") == 10:
+            if int.from_bytes(bytes[i],"little") == 10:
                 nmea_end = True
             else:
                 i+=1
@@ -124,12 +97,10 @@ def check_bytes(ser):
             return 1
 
 
-def perpenddicular_distance(base_vector,vecrover):
-    lineardistance = math.sqrt(vecrover[0]**2+vecrover[1]**2)
-    perpdist = math.sqrt(lineardistance**2+math.sqrt(base_vector[0]**2+base_vector[1]**2))
-    print(perpdist)
-    print(lineardistance)
-    print(math.sqrt(30**2))
+def perpendicular_distance(base_ned, boat_ned):
+    perpdist = abs((base_ned[1] * boat_ned[0] + base_ned[0] * boat_ned[1])) / (math.sqrt(base_ned[0] ** 2 + base_ned[1] ** 2))
+    print("Perpendicular distance is: ", perpdist)
+
 
 def main():
     s = createserialcommunication()
@@ -137,16 +108,22 @@ def main():
     while checkreturn != 1:
         checkreturn = check_bytes(s)
 
-    base_vector = ubxnavrelposned(s)
+    base_vector =ubx_messages.ubxnavrelposned(s)
     #base_vector = [-1684.44, -2476.23, 37.8]
     print(base_vector)
     input("Press any key to begin")
    #base_vector = ubxnavrelposned(s)
     has_crossed_line(base_vector,s)
-    print("Receiver has crossed line at ",)
+    print("Receiver has crossed line")
 
     return 0
 
+def simulation():
+    base_ned = [100, 100, 0]
+    boat_ned = [-10, 0, 0]
+    angle_between(base_ned, boat_ned)
+    perpendicular_distance(base_ned, boat_ned)
+    dist_line_segment(base_ned, boat_ned)
 
 if __name__ == "__main__":
-    main()
+    simulation()
---
+++ ubx_messages.py
@@ -0,0 +1,65 @@
+import numpy as np
+import serial
+import struct
+import time
+
+def ubxnavposllh(ser):
+    time.sleep(.1)
+    bytes2 = b'\xb5b\x01\x02\x1c\x00\x88l\xcf\x1d0\x16K\xfcL\xf6\xc5\x1fNY\x01\x00\xe3\x8a\x00\x00\xd0\x08\x00\x00\x1c' \
+            b'\x13\x00\x00 '
+    bytes = ser.read(34)
+    print(bytes)
+    headerbyte1 = bytes[0]
+    headerbyte2 = bytes[1]
+    overhead = bytes[2]
+    overhead2 = bytes[3]
+    message_size = int.from_bytes(bytes[4:6],"little")
+    timeweek = int.from_bytes(bytes[6:10],"little")
+    longitude = struct.unpack("<i",bytes[10:14])
+    revisedlongitude = longitude[0] * 1e-7
+    latitude = struct.unpack("<I",bytes[14:18])
+    #latitude = int.from_bytes(bytes[14:18],"little")
+    revisedlatitude = latitude[0]*1e-7
+    heightellipsoid = int.from_bytes(bytes[18:22],"little")
+    heightmsl = int.from_bytes(bytes[22:26],"little")
+    horiaccuracy = int.from_bytes(bytes[26:30],"little")
+    vertaccuracy = int.from_bytes(bytes[30:34],"little")
+    print(revisedlongitude)
+
+
+def ubxnavrelposned(ser):
+    bytes = ser.read(46)
+    #bytes2=b'\xb5b\x01<(\x00\x00\x00\x00\x00\x88r3"*\x01\x00\x00\xe5\x01\x00\x00B\xff\xff\xff68\xde\x00\xee\x0e\x00\x00'
+    #headerbyte1 = bytes[0]
+    #headerbyte2 = bytes[1]
+    #class_byte = bytes[0]
+    #id = bytes[1]
+    #message_size = int.from_bytes(bytes[2:4], "little")
+    #message_version = bytes[4]
+    #reserved = bytes[5]
+    #ref_station_id = bytes[6:8]
+    #timeweek = int.from_bytes(bytes[8:12], "little")
+    relposn = struct.unpack("<l",bytes[12:16])
+    relpose = struct.unpack("<l", bytes[16:20])
+    relposd = struct.unpack("<l", bytes[20:24])
+    relposhpn = np.uintc(bytes[24]) * 1e-2
+    relposhpe = np.uintc(bytes[25]) * 1e-2
+    relposhpd = np.uintc(bytes[26]) * 1e-2
+    #reserved2 = bytes[27]
+    #accn =int.from_bytes(bytes[28:32], "little")
+    #acce = int.from_bytes(bytes[32:36], "little")
+    #accd = int.from_bytes(bytes[36:40], "little")
+    #flags = bytes[40:44]
+    #check1 = bytes[44];
+    #check2 = bytes[45];
+
+    resolvedn = relposn[0] + relposhpn
+    resolvede = relpose[0] + relposhpe
+    resolvedd = relposd[0] + relposhpd
+
+
+    #lineardistance = math.sqrt(resolvedn**2+resolvede**2)/100
+    #print(lineardistance)
+    return [resolvedn, resolvede, resolvedd]
+
+
